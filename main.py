82c17d924f23f315c667372b746376aae8afdae1
Octavio <octa7diaz@gmail.com>
2021-03-28 12:53:59 -0400

    letter variable put in

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from camera import VideoCamera
 
 # SOURCE: https://medium.datadriveninvestor.com/video-streaming-using-flask-and-opencv-c464bf8473d6
 
+global letter
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,6 +16,7 @@ def index():
 def gen(camera): 
     while True:
          #get camera frame
+        letter=camera.get_letter()
         frame = camera.get_frame() #initializes object and uses get_frame attribute
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
@@ -24,18 +27,16 @@ def gen(camera):
 @app.route('/video_feed')
 def video_feed():
     #shows video feed to user
-    
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/Model.html', methods=["GET", "POST"])
 def model():
-    return render_template('Model.html', letter = camera.get_letter())
+    return render_template('Model.html', letter)
     #renders HTML Template
 
+    #renders HTML Template
 
-    
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
